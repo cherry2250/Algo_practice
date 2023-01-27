dadcf709787d75efcd6cf029b2c252eb92400a77
chaeri kim <kcr7812@naver.com>
2023-01-27 15:38:21 +0900

    0127

---
+++ 백준/2839.py
@@ -0,0 +1,13 @@
+N = int(input())
+ans = 0
+while N >= 0:
+    if N % 5 == 0:
+        ans += (N // 5)
+        break
+    else :
+        N -= 3
+        ans += 1
+if N < 0:
+    print(-1)
+else :
+    print(ans)
