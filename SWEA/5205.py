237d7f9258ec9641683b33d1fa3b266516873fa8
chaeri kim <kcr7812@naver.com>
2023-01-31 20:00:25 +0900

    0131

---
+++ SWEA/5205.py
@@ -0,0 +1,31 @@
+#퀵정렬
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    low, equal, high = [], [], []
+    for num in arr:
+        if num < pivot:
+            low.append(num)
+        elif num > pivot:
+            high.append(num)
+        else:
+            equal.append(num)
+    return quick_sort(low) + equal + quick_sort(high)
+    
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    ans = quick_sort(arr)
+    print(f'#{tc} {ans[N//2]}')
+
+
+#정렬 라이브러리
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = sorted(list(map(int, input().split())))
+    ans = arr[N//2]
+    print(f'#{tc} {ans}')
