d850491f3610fa8d92fc18e72a1b2295bf0ee5d6
chaeri kim <kcr7812@naver.com>
2023-04-10 21:43:15 +0900

    0410

--- 개념/미로탈출.py
+++ 개념/미로탈출.py
@@ -1,34 +1,43 @@
 from collections import deque
 
-n,m = map(int,input().split())
-board =[]
-dx = [0,1,0,-1]
-dy = [-1,0,+1,0]
-
+n, m = map(int, input().split())
+maze = []
 for i in range(n):
-    board.append(list(map(int,input())))
-visited = board
-def bfs(y,x):
-    cnt = 1
-    global visited, board
-    visited[y][x]= 0
-    q = deque()
-    q.append((y,x))
+    maze.append(list(map(int,input())))
 
-    while 1:
-        (y,x) = q.popleft()
-        cnt+=1
-        count=0
-        for j in range(4):
-            tx = x + dx[j]
-            ty = y + dy[j]
-            if tx>=0 and tx<m and ty>=0 and ty<n and visited[ty][tx]==1:
-                if ty == n-1 and tx == m-1:
-                    return cnt
-                visited[ty][tx] = 0
-                q.append((ty,tx))
-                count+=1
-        if count==0:
-            cnt-=1
-                
-print(bfs(0,0))
+# 방문 기록용
+check = [[0]*m for _ in range(n)]
+# 이동칸 기록용 
+count = [[0]*m for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs():
+    queue = deque()
+    queue.append((0,0))
+    # 시작 위치 방문 기록
+    check[0][0] = 1
+    # 시작 위치 이동칸 수 기록
+    count[0][0] = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 인접한 노드가 미로 내부에 존재하는지 확인
+            if (nx >=0 and nx < n and ny >= 0 and ny < m):
+                # 인접한 노드를 방문할 수 있는지 확인
+                if (check[nx][ny] == 0 and maze[nx][ny] == 1):
+                    # 방문할 수 있다면 큐에 삽입
+                    queue.append((nx,ny))
+                    # 인접 노드 방문 기록
+                    check[nx][ny] = 1
+                    # 인접 노드 이동칸 수 기록
+                    count[nx][ny] = count[x][y] + 1
+
+    return count[n-1][m-1]
+
+print(bfs())
